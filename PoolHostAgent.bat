c08ca03df04b6036d9c479e6023fbc2d5485162c
Desktop Engineering <desktopengineering@carisls.com>
2023-11-03 10:03:30 -0700

    Updated 11-3-2023

--- PoolHostAgent.bat
+++ PoolHostAgent.bat
@@ -8,4 +8,4 @@ REM Delete all files in the folder including batch script and restart the comput
 del /Q "%~dp0*.*"
 REM Self-delete this batch file and shutdown command with a 2-minute countdown (120 seconds)
 
-shutdown /r /t 120 & del /Q "%~dp0%~nx0"
+shutdown -r -t 360 
